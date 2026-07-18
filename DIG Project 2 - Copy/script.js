932453dbdc0801bd1ca9874f243b4f1543260530
Reeo-1 <riondrofercilien@gmail.com>
2026-07-17 23:16:58 -0400

    Add files via upload

---
+++ DIG Project 2 - Copy/script.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("selectBtn");
+let title = document.getElementById("title");
+
+button.addEventListener("click", function() {
+
+    title.style.fontSize = "60px";
+
+});
